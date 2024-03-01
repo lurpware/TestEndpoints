47d6e825816277723d28b0e8e4d5a443ab4b40b1
Matt Robertson <matt.robertson79@gmail.com>
2024-03-01 09:52:00 -0700

    Added Swagger Documentation

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,46 +3,68 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TestWebApp.Controllers
 {
+	/// <summary>
+	/// The basic API Endpoints
+	/// </summary>
 	[ApiController]
 	[Route("[controller]")]
 	public class TestController : ControllerBase
 	{
 		/// <summary>
+		/// Simulates a simple GET request
 		/// Summary
 		/// </summary>
-		/// <param name="sleepTime">Amount of time to wait before returning</param>
-		/// <param name="statusCode">Status code to return</param>
+		/// <param name="sleepTime">Amount of time, in miiliseconds, to wait before returning</param>
+		/// <param name="statusCode">The http status code to return</param>
 		/// <param name="body">Body to return</param>
 		/// <returns></returns>
 		[HttpGet(Name = "Get")]
-		public IActionResult TestGet([FromQuery] int sleepTime, [FromQuery] int statusCode = 200, [FromQuery] string body="hello")
+		public IActionResult TestGet([FromQuery] int sleepTime = 10, [FromQuery] int statusCode = 204, [FromQuery] string? body = null)
 		{
 			Thread.Sleep(sleepTime);
 			return StatusCode(statusCode, body);
 		}
 
+		/// <summary>
+		/// Simulates a simple POST request
+		/// Summary
+		/// </summary>
+		/// <param name="sleepTime">Amount of time, in miiliseconds, to wait before returning</param>
+		/// <param name="statusCode">The http status code to return</param>
+		/// <param name="body">Body to return</param>
+		/// <returns></returns>
 		[HttpPost(Name = "Post")]
-		public StatusCodeResult TestPost([FromQuery] int sleepTime, [FromQuery] int statusCode = 204)
-		{
-			Thread.Sleep(sleepTime);
-			return new StatusCodeResult(statusCode);
-		}
-
-		[HttpPut(Name = "Put")]
-		public StatusCodeResult TestPut([FromQuery] int sleepTime, [FromQuery] int statusCode = 201)
+		public StatusCodeResult TestPost([FromQuery] int sleepTime = 10, [FromQuery] int statusCode = 204, [FromQuery] string? body = null)
 		{
 			Thread.Sleep(sleepTime);
 			return new StatusCodeResult(statusCode);
 		}
 
 		/// <summary>
-		/// Simulates a HTTP DELETE call
+		/// Simulates a simple PUT request
+		/// Summary
 		/// </summary>
-		/// <param name="sleepTime">The amout of time the call will wait bedore returning</param>
-		/// <param name="statusCode"></param>
+		/// <param name="sleepTime">Amount of time, in miiliseconds, to wait before returning</param>
+		/// <param name="statusCode">The http status code to return</param>
+		/// <param name="body">Body to return</param>
+		/// <returns></returns>
+		[HttpPut(Name = "Put")]
+		public StatusCodeResult TestPut([FromQuery] int sleepTime = 10, [FromQuery] int statusCode = 201, [FromQuery] string? body = null)
+		{
+			Thread.Sleep(sleepTime);
+			return new StatusCodeResult(statusCode);
+		}
+
+		/// <summary>
+		/// Simulates a simple DELETE request
+		/// Summary
+		/// </summary>
+		/// <param name="sleepTime">Amount of time, in miiliseconds, to wait before returning</param>
+		/// <param name="statusCode">The http status code to return</param>
+		/// <param name="body">Body to return</param>
 		/// <returns></returns>
 		[HttpDelete(Name = "Delete")]
-		public StatusCodeResult TestDelete([FromQuery] int sleepTime, [FromQuery] int statusCode = 204)
+		public StatusCodeResult TestDelete([FromQuery] int sleepTime = 10, [FromQuery] int statusCode = 204, [FromQuery] string? body = null)
 		{
 			Thread.Sleep(sleepTime);
 			return new StatusCodeResult(statusCode);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+	
+	c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Test Endpoints",
+		Description = "A collection of API Endpoints that are usefull for testing applications that hit API enpoints."
+	});
+
+	var filePath = Path.Combine(AppContext.BaseDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".xml");
+	c.IncludeXmlComments(filePath);
+});
 
 builder.Services.Configure<Microsoft.AspNetCore.Mvc.ApiBehaviorOptions>(options =>
 {
